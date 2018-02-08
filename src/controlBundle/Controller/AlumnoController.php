6e8ffd49798b21b68e855d9d2a31c2a166e35067
selobu <sergiolozanobueno@gmail.com>
2018-02-08 13:19:24 +0100

    Rutas base terminadas

--- src/controlBundle/Controller/AlumnoController.php
+++ src/controlBundle/Controller/AlumnoController.php
@@ -19,7 +19,7 @@ class AlumnoController extends Controller
     }
 
     /**
-     * @Route("/tabla")
+     * @Route("/tabla" , name="listalumnos")
      */
     public function tablaAction()
     {
