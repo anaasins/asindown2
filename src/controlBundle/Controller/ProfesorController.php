11b5d4b5760029c17d8fc5b9687ef0c9a64f1279
guille777 <gbrfloridaa@gmail.com>
2018-02-08 10:54:40 +0100

    ampliacion de profesores

--- src/controlBundle/Controller/ProfesorController.php
+++ src/controlBundle/Controller/ProfesorController.php
@@ -4,8 +4,36 @@ namespace controlBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use controlBundle\Entity\profesor;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProfesorController extends Controller
 {
+   /**
+   * @Route("/listaprofesores", name="listaprofesores")
+   */
+  public function listaprofesores(Request $request)
+  {
+      // replace this example code with whatever you need
+      $repository = $this->getDoctrine()->getRepository(profesor::class);
+      // find *all* jugadores
+      $prof = $repository->findAll();
+      return $this->render('profesores/profesores.html.twig',array("prof"=>$prof));
+  }
+  /**
+  * @Route("/id/{id}", name="id")
+  */
+  public function idAction($id){
+      $repository = $this->getDoctrine()->getRepository('controlBundle:profesor');
+      // find *id* cerveza
+      $prof = $repository->findOneById($id);
+        if(!$prof){
+          return $this->render('profesores/error.html.twig');
+        }
+        // hacer para que si esta vacio cuando llegue a id.twig
+        return $this->render('profesores/id.html.twig',array("prof"=>$prof));
+    }
+
 
 }
