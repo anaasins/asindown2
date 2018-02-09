f178b0eab4a8962d630e6cf25c16afc2f47f0137
guille777 <gbrfloridaa@gmail.com>
2018-02-09 11:17:51 +0100

    crud cursos

--- src/controlBundle/Controller/CursoController.php
+++ src/controlBundle/Controller/CursoController.php
@@ -7,9 +7,22 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use controlBundle\Entity\curso;
 use controlBundle\Form\cursoType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CursoController extends Controller
 {
+  /**
+  * @Route("/listarCursos", name="listarCursos")
+  */
+  public function listarCursos(Request $request)
+  {
+      // replace this example code with whatever you need
+      $repository = $this->getDoctrine()->getRepository(curso::class);
+      // find *all* cursos
+      $curso = $repository->findAll();
+      return $this->render('cursos/listarCursos.html.twig',array("curso"=>$curso));
+  }
+
   /**
    * @Route("/nuevoCurso", name="nuevoCurso")
    */
@@ -29,4 +42,64 @@ class CursoController extends Controller
  }
       return $this->render('cursos/nuevoCurso.html.twig', array('form'=>$form->createView()));
   }
+
+  /**
+  * @Route("/eliminarCurso/{id}", name="eliminarCurso")
+  */
+  public function eliminarCursoAction($id)
+  {
+            $DB = $this->getDoctrine()->getManager();
+
+            $eliminar = $DB->getRepository('controlBundle:curso')->find($id);
+
+            if (!$eliminar) {
+                // throw $this->createNotFoundException('No se ha encontrado el id: '.$id);
+                return $this->render('controlBundle:cursos:error.html.twig');
+            }
+            $DB->remove($eliminar);
+            $DB->flush();
+
+        return $this->redirectToRoute('listarCursos');
+    }
+
+     /**
+    * @Route("/cursoActualizar/{id}", name="cursoActualizar")
+    */
+    public function cursoActualizarAction(Request $request,$id)
+    {
+
+      $curso = $this->getDoctrine()->getRepository('controlBundle:curso')->find($id);
+
+        if(!$curso){return $this->redirectToRoute('error');}
+        $form = $this->createForm(\controlBundle\Form\cursoType::class, $curso);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $DB = $this->getDoctrine()->getManager();
+            $DB->persist($curso);
+            $DB->flush();
+            return $this->redirectToRoute('listarCursos');
+        }
+        return $this->render("cursos/cursoActualizar.html.twig", array('form'=>$form->createView() ));
+    }
+
+
+    /**
+    * @Route("/error", name="error")
+    */
+    public function errorAction()
+    {
+        return $this->render('controlBundle:cursos:error.html.twig');
+    }
+
+
+
+
+
+
+
+
+
+
 }
