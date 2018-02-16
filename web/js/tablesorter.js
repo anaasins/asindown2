cd9ed271f1a00bc15efed3800f3bf64b5c94b944
Ana Asins <asinsanna@gmail.com>
2018-02-16 08:48:55 +0100

    tablesorter comentado

--- web/js/tablesorter.js
+++ web/js/tablesorter.js
@@ -2,49 +2,45 @@ function sortTable(n) {
   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
   table = document.getElementById("regTable");
   switching = true;
-  // Set the sorting direction to ascending:
+  // Asignar la direccion de ordenado a ascendiente
   dir = "asc";
-  /* Make a loop that will continue until
-  no switching has been done: */
   while (switching) {
-    // Start by saying: no switching is done:
+    // Ponemos switching a falso
     switching = false;
     rows = table.getElementsByTagName("TR");
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
+    /* Recorrer todas las lineas de la table */
     for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
+      // Ponemos shouldSwitch a falso
       shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
+      /* Cogemos un elemento de la linea donde estamos y otro de la siguiente para
+      compararlos */
       x = rows[i].getElementsByTagName("TD")[n];
       y = rows[i + 1].getElementsByTagName("TD")[n];
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
+      /* Comprobamos si las dos filas comparadas deben de cambiar el posicion
+      y si es de forma ascendente o descendiente */
       if (dir == "asc") {
         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
+          // si es asi, marcas el shouldSwitch y salimos
           shouldSwitch = true;
           break;
         }
       } else if (dir == "desc") {
         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
+          // si es asi, marcas el shouldSwitch y salimos
           shouldSwitch = true;
           break;
         }
       }
     }
     if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
+      /* Si el switch ha sido marcado, lo hacemos
+      y marcamos que se ha hecho */
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
-      // Each time a switch is done, increase this count by 1:
+      // Cada vez que se hace un cambio, subimos el switch count en 1
       switchcount++;
     } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
+      /* Si no se hace cambio y la direccion es ascendiente, la marcamos como descendiente */
       if (switchcount == 0 && dir == "asc") {
         dir = "desc";
         switching = true;
